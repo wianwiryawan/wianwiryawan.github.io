223a5783c311b5941d1597541d2221b2c9a3bbde
wiyan <wianwiryawan@gmail.com>
2025-06-21 17:35:27 +0700

    Add hero page section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import { Header } from "@/components/header";
+import { Hero } from "@/components/hero";
 
 export default function Portofolio() {
   return (
     <div className="min-h-screen bg-background">
       <Header />
       <main>
+        <Hero />
       </main>
     </div>
   );
